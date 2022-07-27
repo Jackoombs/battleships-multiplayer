a687cf7b6cee2142f4be7f88a7d29f0c985db1f8
Jackoombs <jack.coombs@hotmail.co.uk>
2022-07-27 16:01:59 +0100

    Added server address.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Game from "./components/Game";
 import Result from "./components/Result";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080/");
+const socket = io("https://jc-battleships.herokuapp.com/");
 
 function App() {
   const [playerTurn, setPlayerTurn] = useState();
