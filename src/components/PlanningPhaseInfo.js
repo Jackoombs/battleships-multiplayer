7802cae0fd41530275b97eb3f782f2414fa88c09
Jackoombs <jack.coombs@hotmail.co.uk>
2022-07-29 15:00:47 +0100

    Added additional instructions.

--- src/components/PlanningPhaseInfo.js
+++ src/components/PlanningPhaseInfo.js
@@ -5,7 +5,10 @@ function PlanningPhaseInfo() {
     <div>
       <h2>Planning Phase</h2>
       <ul>
-        <li>Decide where you ships be positioned during the battle.</li>
+        <li>Decide where your ships will be positioned during the battle.</li>
+        <li>Use the space-bar or scroll to rotate your ship or the rotate icon on mobile.</li>
+        <li>Once all your ships are placed, click a ship if you'd like to reposition it.</li>
+        <li>When you're ready click the 'BEGIN BATTLE' button.</li>
       </ul>
     </div>
   );
